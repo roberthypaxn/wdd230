4ebe66f8db2bbc4f494824d60591169c8973cc2c
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-01-29 07:31:50 +0200

    Created scripts and styles folders

---
+++ chamber/scripts/date.js
@@ -0,0 +1,16 @@
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
