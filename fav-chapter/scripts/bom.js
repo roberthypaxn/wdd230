803c003015ae53865aa6b71ced0aab816e69b78b
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-02-03 09:21:25 +0200

    added a case for an empty input

--- fav-chapter/scripts/bom.js
+++ fav-chapter/scripts/bom.js
@@ -1,18 +1,25 @@
 const button = document.querySelector("button");
 const inputField = document.querySelector("#favchap");
 const list = document.querySelector("#list");
+const noInput = document.querySelector("#noInput");
 
 button.addEventListener("click", function () {
-	const inputValue = inputField.value;
-	const listItem = document.createElement("li");
-	listItem.textContent = inputValue;
-	
-	const deleteButton = document.createElement("button");
-	deleteButton.textContent = "Delete";
-	deleteButton.addEventListener("click", function() {
-	  list.removeChild(listItem);
-	});
-	listItem.appendChild(deleteButton);
-  
-	  list.appendChild(listItem);
+  const inputValue = inputField.value;
+  if (!inputValue) {
+    noInput.textContent = "Please enter a valid favorite chapter.";
+    return;
+  }
+
+  noInput.textContent = "";
+  const listItem = document.createElement("li");
+  listItem.textContent = inputValue;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", function() {
+    list.removeChild(listItem);
+  });
+  listItem.appendChild(deleteButton);
+
+  list.appendChild(listItem);
 });
