215958b3d7c9dfdcb1fbc163dbb30e5dc472bf55
Robert Hypax Ndiramiye <ndiramiye.robert@gmail.com>
2023-04-06 16:04:44 +0200

    works

--- Bountiful-Foods/scripts/weather.js
+++ Bountiful-Foods/scripts/weather.js
@@ -1,92 +1,62 @@
-const currentTemp = document.getElementById("currentTemp");
-const weath = document.getElementById("weath");
-const humidity = document.getElementById("humidity");
+const temp = document.querySelector("#temp");
+const weatherIcon = document.querySelector("#w-icon");
+const weatherDesc = document.querySelector("#weather-description");
 
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=b7343d2bf8aba69528ad671e5fd70f04";
-const newUrl =
-  "https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&units=imperial&appid=b7343d2bf8aba69528ad671e5fd70f04";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=metric&appid=8633852aefcacbfa429c5839142459db";
+const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=33.1581&lon=-117.3506&cnt=3&units=imperial&appid=8633852aefcacbfa429c5839142459db";
 
 async function apiFetch() {
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      displayResults(data);
-    } else {
-      throw Error(await response.text());
-    }
-    const resp = await fetch(newUrl);
-    if (resp.ok) {
-      const dailyData = await resp.json();
-      console.log(dailyData);
-      displayCards(dailyData);
-    } else {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+      const resp = await fetch(forecastUrl);
+      if (resp.ok) {
+        const forecastData = await resp.json();
+        console.log(forecastData);
+        displayForecast(forecastData);
+      } else {
       throw Error(await resp.text());
+      }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
   }
-}
+  
 apiFetch();
 
 function displayResults(weatherData) {
-  // Current Temperature
-  currentTemp.innerHTML = `${weatherData.main.temp.toFixed(1)} °F`;
-  // Weather Description
-  const desc = weatherData.weather[0].description;
-  let newDesc = desc.split(" ").map(capitalize).join(" ");
-  weath.innerHTML = newDesc;
-  // Humidity
-  humidity.innerHTML = `${weatherData.main.humidity}`;
-}
+    temp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>` + "°C";
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
 
-function capitalize(s) {
-  return `${s.charAt(0).toUpperCase()}${s.slice(1)}`;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    weatherDesc.textContent = desc;
 }
-
-function displayCards(weatherData) {
+function displayForecast(weatherData) {
   const divCards = document.getElementById("weather-cards");
-  let indexWeather = 0  
   for (let i = 0; i < 3; i++) {
-    indexWeather += 8;
+    
     const divContainer = document.createElement("div");
     divContainer.setAttribute("class", "card-weather");
-    const nextDate = document.createElement("span");
-    const cardCurrent = document.createElement("span");
-    const cardWeath = document.createElement("span");
+    const forecast1 = document.createElement("span");
+    const forecast2 = document.createElement("span");
+    const forecast3 = document.createElement("span");
 
-    nextDate.innerHTML = timeConverter(weatherData.list[indexWeather].dt);
-    cardCurrent.innerHTML = `${weatherData.list[indexWeather].main.temp.toFixed(0)}°F`;
-    cardWeath.innerHTML = weatherData.list[indexWeather].weather[0].main.split(" ").map(capitalize).join(" ");
+    forecast1.innerHTML = weatherData.list[i].dt_txt;
+    forecast2.innerHTML = `${weatherData.list[i].main.temp.toFixed(0)}°C`;
+    forecast3.innerHTML = weatherData.list[i].weather[0].description;
 
-    divContainer.appendChild(nextDate);
-    divContainer.appendChild(cardCurrent);
-    divContainer.appendChild(cardWeath);
+    divContainer.appendChild(forecast1);
+    divContainer.appendChild(forecast2);
+    divContainer.appendChild(forecast3);
 
     divCards.appendChild(divContainer);
   }
-}
-
-function timeConverter(UNIX_timestamp) {
-  var a = new Date(UNIX_timestamp * 1000);
-  var months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var time = month + " " + date;
-  return time;
 }
